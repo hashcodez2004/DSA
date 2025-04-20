c656c25e46e2db446644306c267be6dd84a2a698
Harsh Sharma <kumarsharmaharsh2004@gmail.com>
2025-04-20 16:51:43 +0530

    0/1 Knapsack--TABULATED

---
+++ 0-1 Knapsack--TABULATED.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int knapsack(int W, const vector<int>& wt, const vector<int>& val) {
+        int n = wt.size();
+        vector<vector<int>> dp(n, vector<int>(W+1,0));
+        for(int i=0;i<=W;i++){
+            if(i>=wt[0]) dp[0][i]=val[0];
+        }
+
+        for(int idx=1;idx<n;idx++){
+            for(int weight=1;weight<=W;weight++){
+                int notTake = dp[idx-1][weight];
+                int take = 0;
+                if(wt[idx]<=weight) take = val[idx] + dp[idx-1][weight-wt[idx]];
+                dp[idx][weight] = max(take,notTake);
+            }
+        }
+        return dp[n-1][W];
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, W;
+        cin >> n >> W;
+        vector<int> val (n);
+        for (int i = 0; i < n; i++) cin >> val[i];
+        vector<int> wt(n);
+        for (int i = 0; i < n; i++) cin >> wt[i];
+
+        Solution ob;
+        int s = ob.knapsack(W, wt, val);
+        cout << s << endl;
+    }
+    return 0;
+}
+
+
+/*
+Time Complexity: O(n x W)
+Space Complexity: O(n x W) [for DP table]
+*/
