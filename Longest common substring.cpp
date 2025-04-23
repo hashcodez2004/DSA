914d44ee72834b7e954d21dff9ee7d7fc669826f
Harsh Sharma <kumarsharmaharsh2004@gmail.com>
2025-04-23 23:56:43 +0530

    Longest common substring

---
+++ Longest common substring.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestCommonSubstring(string s1, string s2) {
+        int size1 = s1.size();
+        int size2 = s2.size();
+        vector<vector<int>> dp(size1+1,vector<int>(size2+1,0));
+        int maxi = 0;
+        for(int i=1;i<=size1;i++){
+            for(int j=1;j<=size2;j++){
+                if(s1[i-1]==s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = 0;
+                maxi = max(maxi,dp[i][j]);
+            }
+        }
+        return maxi;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+        Solution ob;
+        int s = ob.longestCommonSubstring(s1,s2);
+        cout << s << endl;
+    }
+    return 0;
+}
+
+
+/*
+Time Complexity: O(n * m)
+Space Complexity: O(n * m)
+*/
