2684c70077df17e56d8497b02123c28f1a99b209
hashcodez2004 <113353906+hashcodez2004@users.noreply.github.com>
2025-08-03 02:33:32 +0530

    Update Best Time to Buy and Sell Stock II--TABULATED.cpp

--- Best Time to Buy and Sell Stock II--TABULATED.cpp
+++ Best Time to Buy and Sell Stock II--TABULATED.cpp
@@ -6,7 +6,7 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<int>> dp(n+1, vector<int>(2, 0));
-        dp[n][0] == dp[n][1] == 0;
+        dp[n][0] = dp[n][1] = 0;
         for(int idx = n-1; idx>=0; idx--){
             for(int buy = 1; buy>=0; buy--){
                 int profit = 0;
