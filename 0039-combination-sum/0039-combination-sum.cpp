1254327f586862f68ae1252293ebcea0c25ae7a3
hashcodez2004 <113353906+hashcodez2004@users.noreply.github.com>
2025-08-21 22:56:26 +0530

    Time: 5 ms (35.6%), Space: 17.7 MB (30.1%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,18 +1,20 @@
 class Solution {
 private:
     void helper(int idx, int target, vector<int>& v, vector<vector<int>>& ans, vector<int>& candidates){
-        if(target<0) return;
-        if(idx==candidates.size()){
-            if(target==0) ans.push_back(v);
+        if (target == 0) {
+            ans.push_back(v);
             return;
         }
+        if (idx == candidates.size() || target < 0) return;
 
         //notTake
         helper(idx+1,target,v,ans,candidates);
         //take
-        v.push_back(candidates[idx]);
-        helper(idx,target-candidates[idx],v,ans,candidates);
-        v.pop_back();
+        if (target >= candidates[idx]) {  //If possible
+            v.push_back(candidates[idx]);
+            helper(idx, target - candidates[idx], v, ans, candidates);
+            v.pop_back();
+        }
     }
 
 public:
