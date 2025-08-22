c9a2f570c9278af2ab669430807ba136809f1dcd
hashcodez2004 <113353906+hashcodez2004@users.noreply.github.com>
2025-08-22 22:34:28 +0530

    Time: 0 ms (100%), Space: 9.1 MB (29.75%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,27 +1,26 @@
 class Solution {
 private:
-    void helper(int idx, int sum, int k, vector<int>& v, int n, vector<vector<int>>& ans, vector<int>& nums){
+    void helper(int idx, int sum, int k, vector<int>& v, vector<vector<int>>& ans){
         if(k==0){
-            if(sum==n)
-            ans.push_back(v);
+            if(sum==0) ans.push_back(v);
             return;
         }
-        if(sum>n || idx==nums.size()) return;
-        //notTake
-        helper(idx+1,sum,k,v,n,ans,nums);
-        //Take
-        v.push_back(nums[idx]);
-        helper(idx+1,sum+nums[idx],k-1,v,n,ans,nums);
+        if(sum<0 || idx>9) return;
+
+        // notTake
+        helper(idx+1,sum,k,v,ans);
+
+        // take
+        v.push_back(idx);
+        helper(idx+1,sum-idx,k-1,v,ans);
         v.pop_back();
     }
 
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> nums(9);
-        for(int i=0;i<9;i++) nums[i]=i+1;
         vector<vector<int>> ans;
         vector<int> v;
-        helper(0,0,k,v,n,ans,nums);
+        helper(1,n,k,v,ans);
         return ans;
     }
 };
