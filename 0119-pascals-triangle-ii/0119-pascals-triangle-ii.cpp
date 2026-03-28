5b171898454e12f3d77d183cb4b21564dd2c6cd9
hashcodez2004 <113353906+hashcodez2004@users.noreply.github.com>
2026-03-28 11:01:22 +0530

    Time: 0 ms (100%), Space: 8.8 MB (73.64%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    vector<int> getRow(int rowIndex) {
-        if(rowIndex == 0) return {1};
-        if(rowIndex == 1) return {1,1};
-
-        vector<int> prev = {1,1};
-
-        for(int i = 2; i <= rowIndex; i++){
-            vector<int> curr(i+1);
-            curr[0] = curr[i] = 1;
-            
-            for(int j = 1; j < i; j++){
-                curr[j] = prev[j-1] + prev[j];
-            }
-
-            prev=curr;
+    vector<int> getRow(int row) {
+        row=row+1; //did this because of 0 indexed row
+        vector<int> ans;
+        ans.push_back(1);
+        long long temp=1;
+        for(int col=1;col<row;col++){
+            temp=temp*(row-col);
+            temp=temp/col;
+            ans.push_back(temp);
         }
-        
-        return prev;
+        return ans;
     }
 };
