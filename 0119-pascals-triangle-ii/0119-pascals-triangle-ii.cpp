6c151eb44185f681c97b0dcc6ae7222ff10580aa
hashcodez2004 <113353906+hashcodez2004@users.noreply.github.com>
2026-03-28 09:15:04 +0530

    Time: 0 ms (100%), Space: 9 MB (32.99%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        if(rowIndex == 0) return {1};
+        if(rowIndex == 1) return {1,1};
+
+        vector<int> prev = {1,1};
+
+        for(int i = 2; i <= rowIndex; i++){
+            vector<int> curr(i+1);
+            curr[0] = curr[i] = 1;
+            
+            for(int j = 1; j < i; j++){
+                curr[j] = prev[j-1] + prev[j];
+            }
+
+            prev=curr;
+        }
+        
+        return prev;
+    }
+};
