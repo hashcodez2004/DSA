e985c30d2ccb3fedb3e959961a52b5a820f3ca2e
Harsh Sharma <kumarsharmaharsh2004@gmail.com>
2026-06-28 15:47:06 +0530

    Morris Preorder traversal

---
+++ Morris Preorder traversal.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    vector<int> preOrder(Node* root) {
+        // code here
+        Node *curr=root;
+        vector<int> ans;
+        while(curr){
+            //Left part don't exist
+            if(curr->left==NULL){
+                ans.push_back(curr->data);
+                curr=curr->right;
+            }
+            
+            //Left part exists
+            else{
+                Node* temp=curr->left;
+                while(temp->right!=NULL && temp->right!=curr) temp=temp->right;
+                
+                //Left subtree not traversed
+                if(temp->right!=curr){
+                    ans.push_back(curr->data);
+                    temp->right=curr;
+                    curr=curr->left;
+                }
+                //Left subtree already traversed
+                else{
+                    temp->right==NULL;
+                    curr=curr->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
