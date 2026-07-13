28476b26dd91766da2f9c6db274b8e9ec8bececd
Harsh Sharma <kumarsharmaharsh2004@gmail.com>
2026-07-13 13:20:05 +0530

    Topological Sort(BFS) - Kahn's Algo

---
+++ Topological Sort(BFS) - Kahn's Algo.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<int>> adj(V);
+        vector<int> degree(V,0);
+        for(auto &it: edges){
+            int u=it[0];
+            int v=it[1];
+            adj[u].push_back(v);
+            degree[v]++;
+        }
+        
+        queue<int> q;
+        for(int i=0;i<V;i++)
+            if(degree[i]==0)
+                q.push(i);
+        
+        vector<int> ans;        
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto &it: adj[node]){
+                degree[it]--;
+                if(degree[it]==0)
+                    q.push(it);
+            }
+        }
+        
+        return ans;
+    }
+};
