0f5bbe2e69a7a31cfed70c9b2a471561fe03526f
hashcodez2004 <113353906+hashcodez2004@users.noreply.github.com>
2026-08-25 21:03:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.95%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+private:
+    int helper(int n, vector<int> &dp){
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        if(dp[n]!=-1) return dp[n];
+
+        return dp[n] = helper(n-2,dp) + helper(n-1,dp);
+    }
+
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return helper(n,dp);
+    }
+};
