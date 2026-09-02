bb81db19fab1466f8167b7fd924283ca742ee70c
hashcodez2004 <113353906+hashcodez2004@users.noreply.github.com>
2026-09-02 12:40:46 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.79%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -6,26 +6,27 @@ class Solution {
 
         if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1) return 0;
 
-        vector<vector<int>> dp(m, vector<int>(n,0));
-        dp[0][0] = 1;
+        vector<int> prev(n,0);
 
         for(int i=0;i<m;i++){
+            vector<int> curr(n,0);
+
             for(int j=0;j<n;j++){
-                if(i==0 && j==0) continue;
+                if(i==0 && j==0) curr[j]=1;
+                else if(obstacleGrid[i][j]==1) continue;
+                else{
+                    int up = prev[j];
+                    int left=0;
 
-                if(obstacleGrid[i][j]==1) {
-                    dp[i][j] = 0;
-                    continue;
+                    if(j!=0) left = curr[j-1];
+
+                    curr[j] = up + left;
                 }
-
-                int up=0, left=0;
-                if(i!=0) up = dp[i-1][j];
-                if(j!=0) left = dp[i][j-1];
-
-                dp[i][j] = up + left;
             }
+
+            prev = curr;
         }
 
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
