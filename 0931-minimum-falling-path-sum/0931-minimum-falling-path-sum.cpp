c6c7cd587611ee922ce6f182b2811fd89d927275
hashcodez2004 <113353906+hashcodez2004@users.noreply.github.com>
2026-09-08 22:55:53 +0530

    Time: 0 ms (100%), Space: 15 MB (47.84%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
-private:
-    int helper(int row, int col, vector<vector<int>>& matrix, vector<vector<int>> &dp){
-        if(col<0 || col==matrix.size()) return 1e9;
-        if(row==0) return matrix[0][col];
-
-        if(dp[row][col] != 1e9) return dp[row][col];
-
-        int upLeft = matrix[row][col] + helper(row-1,col-1,matrix,dp);
-        int up = matrix[row][col] + helper(row-1,col,matrix,dp);
-        int upRight = matrix[row][col] + helper(row-1,col+1,matrix,dp);
-        
-        return dp[row][col] = min({upLeft,up,upRight});
-    }
-
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
-        vector<vector<int>> dp(n, vector<int>(n,1e9));
+        vector<int> prev = matrix[0];
+
+        for(int row=1;row<n;row++){
+            vector<int> curr(n);
+            for(int col=0;col<n;col++){
+                int upLeft = 1e9;
+                if(col!=0) upLeft = matrix[row][col] + prev[col-1];
+                int up = matrix[row][col] + prev[col];
+                int upRight = 1e9;
+                if(col!=n-1) upRight = matrix[row][col] + prev[col+1];
+                
+                curr[col] = min({upLeft,up,upRight});
+            }
+
+            prev = curr;
+        }
 
         int ans = INT_MAX;
         for(int i=0;i<n;i++){
-            ans = min(ans, helper(n-1, i, matrix, dp));
+            ans = min(ans, prev[i]);
         }
 
         return ans;
